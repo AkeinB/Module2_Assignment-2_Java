46d1f3e880dfb0ab9b70c0141b0c7f0434320dd5
akeinLP <akeinbantin@gmail.com>
2026-03-07 16:50:02 -0500

    ***Sales Representatives**** -Created Allowance variable -Created Parameterized Constructor -Created Getters and Setters

--- src/SalesRep.java
+++ src/SalesRep.java
@@ -7,4 +7,25 @@
 
 
 public class SalesRep {
+
+    private double Allowance;
+
+//    ================PARAMETERIZED CONSTRUCTOR===========
+    public SalesRep(double Allowance) {
+        this.Allowance = Allowance;
+    }
+
+//    SETTER
+    public void setAllowance(double Allowance) {
+        this.Allowance = Allowance;
+    }
+
+//    GETTER
+    public double getAllowance() {
+        return this.Allowance;
+    }
+
+    public void Display(){
+        System.out.println("the allowance for Sale Reps is " + this.Allowance);
+    }
 }
