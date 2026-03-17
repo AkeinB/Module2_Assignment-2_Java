0c16df1e340bd7f4f3262cca60b4322bf2233770
akeinb <akeinbantin@gmail.com>
2026-03-17 16:01:28 -0500

    ************* Georgia's commit **************** -created variables : ManagerSalary and SalesRepSalary. -created CalculateSalary Methods

--- src/SalesRep.java
+++ src/SalesRep.java
@@ -2,28 +2,38 @@
     Zhane Tomlin #
     Chevar Prince #
     Ryan Brown #
-    Georgia Black #1817002195
+    Georgia Black Golding #1817002195
 */
-
 
 public class SalesRep extends StaffMembers{
 
     private double Allowance;
+    private double SalesRepSalary;
 
-//    ================PARAMETERIZED CONSTRUCTOR===========
-    public SalesRep(String FirstName, String LastName, String DeptNumber, double HoursWorked, double Allowance) {
+    //    ================PARAMETERIZED CONSTRUCTOR===========
+    public SalesRep(String FirstName, String LastName, String DeptNumber,
+                    double HoursWorked, double Allowance, double SalesRepSalary) {
         super(FirstName,LastName,DeptNumber,HoursWorked);// calls parent constructor
         this.Allowance = Allowance;
+        this.SalesRepSalary = SalesRepSalary;
     }
 
-//    SETTER
+    //    SETTER
     public void setAllowance(double Allowance) {
         this.Allowance = Allowance;
     }
 
-//    GETTER
+    public void setSalesRepSalary(double SalesRepSalary) {
+        this.SalesRepSalary = SalesRepSalary;
+    }
+
+    //    GETTER
     public double getAllowance() {
         return this.Allowance;
+    }
+
+    public double GetSalesRepSalary() {
+        return this.SalesRepSalary;
     }
 
     @Override
@@ -35,4 +45,9 @@ public class SalesRep extends StaffMembers{
     }
 
     /*Create Method for CalculateSalary here. implement the appropriate formula for the subclass */
+    public double CalculateSalary()
+    {
+        SalesRepSalary = (HoursWorked * 1500) + Allowance;
+        return SalesRepSalary;
+    }
 }
